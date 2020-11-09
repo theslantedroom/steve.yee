a9760770e866c48b7b27b5410eb1a7223c9f0b24
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2020-11-08 22:15:40 -0800

    lms
    test

--- js/LMS.js
+++ js/LMS.js
@@ -16,3 +16,25 @@ var player1 = {
 	},
 };
 
+var p1h = 20;
+
+function peasantTalk(){
+	document.getElementById("tvLog").innerHTML = "The peasant hits you 2";
+
+	alert('The peasant hits you 2');
+
+	player1.health = player1.health - 2;
+
+	document.getElementById("tvLog").innerHTML = "You got hit, Your health is down to " + player1.health;
+
+	document.getElementById("p1health").innerHTML = player1.health;
+};
+
+
+//var randomNumber = Math.round(Math.random()*10);
+
+function rollDie(){
+	randomNumber = Math.floor((Math.random() * 20) + 1);
+	console.log(randomNumber);
+	document.getElementById("rollResult").innerHTML = randomNumber;
+}
