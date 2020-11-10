5ea9088e673ea55a620359fe37cfc32a91eec68f
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2020-11-09 17:11:02 -0800

    combat

--- js/LMS.js
+++ js/LMS.js
@@ -6,15 +6,16 @@ var player1 = {
 	alive: true,
 	healthMa: 20,
 	health: 20,
-	defense: 4,
-	attackRoll: 0,
-	damageBonus: 0,
-	weapon: 'none',
+	defense: 10,
+	attackRoll: 'n/a',
+	damageBonus: 'n/a',
+	weapon: 'Long Sword 1d8',
 
 	stats: function(){
 		alert(player1.name+ ' has ' + player1.health + '/' + player1.healthMa +' health');
 	},
 };
+
 
 
 var enemy = {
@@ -22,17 +23,66 @@ var enemy = {
 	alive: true,
 	healthMa: 20,
 	health: 20,
-	defense: 4,
-	attackRoll: 0,
-	damageBonus: 0,
-	weapon: 'none',
+	defense: 6,
+	attackRoll: 'n/a',
+	damageBonus: 'n/a',
+	weapon: 'Dagger 1d4',
 
 	stats: function(){
 		alert(player1.name+ ' has ' + player1.health + '/' + player1.healthMa +' health');
 	},
 };
 
-var p1h = 20;
+// Bootup stats
+
+
+
+document.getElementById("player1.attackRoll").innerHTML = player1.attackRoll;
+document.getElementById("enemy.attackRoll").innerHTML = enemy.attackRoll;
+
+document.getElementById("player1.damageBonus").innerHTML = player1.damageBonus;
+document.getElementById("enemy.damageBonus").innerHTML = enemy.damageBonus;
+
+document.getElementById("player1.weapon").innerHTML = player1.weapon;
+document.getElementById("enemy.weapon").innerHTML = enemy.weapon;
+
+document.getElementById("player1HealthCounter").innerHTML = player1.health;
+document.getElementById("player1DefenseCounter").innerHTML = player1.defense;
+document.getElementById("enemyHealthCounter").innerHTML = enemy.health;
+document.getElementById("enemyDefenseCounter").innerHTML = enemy.defense;
+
+// document.getElementById("player1.kills").innerHTML = killCount;
+// document.getElementById("enemy.kills").innerHTML = deathCount;
+
+
+
+var killCount = 0;
+var deathCount = 0;
+
+// enemyturn popup
+function enemyTurn(){
+	console.log('enemyturn');
+};
+
+//enemy ATTACK
+
+function enemyAttack(){
+	console.log('enemyattack');
+	document.getElementById("enemyTurn").className = "hidden";
+
+};
+
+
+// next enemy popup
+function nextEnemy(){
+	enemy.health = 20;
+	document.getElementById("enemyHealthCounter").innerHTML = enemy.health;
+	document.getElementById("rollResult").innerHTML = '...';
+	document.getElementById("next").className = "hidden";
+	document.getElementById("result").innerHTML = 'You look around..';
+	document.getElementById("tvLog").innerHTML = "and see an angry peasant";
+
+};
 
 function peasantTalk(){
 	document.getElementById("tvLog").innerHTML = "The peasant hits you 2";
@@ -43,7 +93,7 @@ function peasantTalk(){
 
 	document.getElementById("tvLog").innerHTML = "You got hit, Your health is down to " + player1.health;
 
-	document.getElementById("p1health").innerHTML = player1.health;
+	document.getElementById("player1.health").innerHTML = player1.health;
 };
 
 
@@ -54,19 +104,46 @@ function rollDie(){
 	console.log(randomNumber);
 	document.getElementById("rollResult").innerHTML = randomNumber;
 
+// HIT
 	if (randomNumber > enemyDefense ) {
 		console.log("hit with " + randomNumber);
 		document.getElementById("result").innerHTML = 'HIT';
-		enemy.health = enemy.health - 2;
-		document.getElementById("tvLog").innerHTML = "You  hit the peasant, health is down to " + enemy.health;
-
-		document.getElementById("enemyHealth").innerHTML = enemy.health;
-
+		enemy.health = enemy.health - 5;
+		document.getElementById("tvLog").innerHTML = "You  hit the peasant 5, health is down to " + enemy.health;
+		document.getElementById("enemyHealthCounter").innerHTML = enemy.health;
+	
+// MIss
 	} else	{
 		console.log("miss with " + randomNumber);
 		document.getElementById("result").innerHTML = 'Miss';
+		document.getElementById("tvLog").innerHTML = "You Missed";
 	};
+
+
+// killed him
+	if (enemy.health <= 0) {
+		killCount = killCount + 1;
+		enemy.health = 0;
+		document.getElementById("enemyHealthCounter").innerHTML = enemy.health;
+		document.getElementById("tvLog").innerHTML = "Killed him!";
+		document.getElementById("tvLog").innerHTML = 'The peasant is DEAD, you have killed ' + killCount + ' peasants';
+		document.getElementById("killpopup").innerHTML = killCount;
+		document.getElementById("next").className = "visibleBlock";
+		console.log('killed enemy');
+	} else {
+
+// enemy turn
+		console.log('enemyturn');
+		document.getElementById("enemyTurn").className = "visibleBlock";
+
+
+	};
+
+
+	
 };
+
+
 
 // change Sex
 function changeSexMaleP1(){
