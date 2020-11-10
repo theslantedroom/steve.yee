7f9e3a6adb3da3e18d45212f5ff3e92d3b37af0f
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2020-11-10 15:15:27 -0800

    Update LMS.js

--- js/LMS.js
+++ js/LMS.js
@@ -23,7 +23,7 @@ var enemy = {
 	name: 'Peasant',
 	alive: true,
 	healthMa: 20,
-	health: 1,
+	health: 20,
 	defense: 6,
 	attackBonus: 'n/a',
 	damageBonus: 'n/a',
@@ -304,7 +304,7 @@ function player1AttackRoll(){
 	if (attackRoll > enemy.defense && attackRoll != 20) {
 		console.log('hit enemy');
 
-		document.getElementById("player1DamageLog").innerHTML = 'A nice swing';
+		document.getElementById("player1DamageLog").innerHTML = 'Attacl roll is ' + attackRoll;
 		document.getElementById("player1Damage").className = "visibleBlock";
 		document.getElementById("player1Turn").className = "hidden";
 	
