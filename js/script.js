ee3b00b4d13b622a19ea5e0321d98df40d861893
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2021-03-04 12:20:56 -0800

    stock picker

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function lms() {
+  window.location.href = "https://theslantedroom.github.io/Last-Man-Standing-Mini-Game/";
+}
+
+function trillion() {
+  window.location.href = "https://www.idletrillionaire.com/";
+}
+
+function mmd() {
+  window.location.href = "https://theslantedroom.github.io/MagicMushroomsDirect/";
+}
+
+function stockPicker() {
+  window.location.href = "http://stockpicker-theslantedroom.herokuapp.com/";
+}
+
+function card() {
+  window.location.href = "https://theslantedroom.github.io/ChristmasGreetings/";
+}
